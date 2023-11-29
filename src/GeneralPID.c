a57958828fe7713c8b39886340a95b9beaf0114a
Serramasther Projetos <projetos1@serramasther.local>
2023-11-29 17:01:47 -0300

    anti-windup + documentation

--- src/GeneralPID.c
+++ src/GeneralPID.c
@@ -1,55 +1,38 @@
 #include "GeneralPID.h"
 
-void GeneralPID_init(GeneralPID *self, float sampling_time, float kp, float ki, float kd)
+int GeneralPID_init(GeneralPID *self, float sampling_time, float kp, float ki, float kd)
 {
-}
+#include <stdlib.h> // For memory allocation functions
 
-void GeneralPID_setGains(GeneralPID *self, float kp, float ki, float kd)
-{
-  // store PID gains inserted by the user
-  self->KP = kp;
-  self->KI = ki;
-  self->KD = kd;
-  // calculate discrete coefficients based on sampling
-  float discrete_kp = self->KP;
-  float discrete_ki = self->KI * self->sampling_time * 0.5;
-  float discrete_kd = self->KD * self->sampling_time * 2;
-  // store gains used to calculate the equivalent difference equation
-  self->K1 = discrete_kp + discrete_ki + discrete_kd;
-  self->K2 = 2 * discrete_ki - 2 * discrete_kd;
-  self->K3 = -discrete_kp + discrete_ki + discrete_kd;
-  // store gains without considering integrator coeffiecient in case of output saturation
-  self->K1_anti_windup = discrete_kp + discrete_ki * 0 + discrete_kd;
-  self->K2_anti_windup = 2 * discrete_ki * 0 - 2 * discrete_kd;
-  self->K3_anti_windup = -discrete_kp + discrete_ki * 0 + discrete_kd;
-}
-
-int GeneralPID_setSetpoint(GeneralPID *self, float setpoint);
-
-float GeneralPID_execute(GeneralPID *self, float measurement)
-{
-  // variables definition
-  float output = 0;
-
-  // error calculation
-  float error = self->setpoint - measurement;
-
-  // calculate differences equation
-  if (self->output_n1 < self->max_output)
+  int GeneralPID_init(GeneralPID * self, float sampling_time, float kp, float ki, float kd)
   {
-    output = self->output_n2 + self->K1 * error + self->K2 * self->error_n1 + self->K3 * self->error_n2;
-  }
-  else
-  {
-    output = self->output_n2 + self->K1_anti_windup * error + self->K2_anti_windup * self->error_n1 + self->K3_anti_windup * self->error_n2;
-  }
+    if (self == 0)
+      return 1;
 
-  // store last control values
-  self->output_n2 = self->output_n1;
-  self->output_n1 = output;
-  self->error_n2 = self->error_n1;
-  self->error_n1 = error;
+    // Initialize struct members with provided values
+    self->setpoint = 0.0;
+    self->sampling_time = sampling_time;
+    //GeneralPID_getGains;
+    self->max_output = 0.0;
+    self->min_output = 0.0;
+    self->last_error = 0.0;
+    self->second_last_error = 0.0;
+    self->last_output = 0.0;
+    self->second_last_output = 0.0;
 
-  // return control action
-  return output;
+    // Set function pointers to zero
+    self->setSetpoint = 0;
+    self->getSetpoint = 0;
+    self->setSamplingTime = 0;
+    self->getSamplingTime = 0;
+    self->setGains = 0;
+    self->getGains = 0;
+    self->setMatlabGains = 0;
+    self->getMatlabGains = 0;
+    self->setLimits = 0;
+    self->getLimits = 0;
+    self->execute = 0;
+
+    return 0; // Success
+  }
 }
--- src/GeneralPID.h
+++ src/GeneralPID.h
@@ -1,6 +1,7 @@
-#ifndef GENERAL_PID_H // diretiva de inclusão condicional para evitar múltiplas inclusões do mesmo arquivo de cabeçalho
-#define GENERAL_PID_H
+#ifndef GENERALPID_H
+#define GENERALPID_H
 
+/// @brief General PID struct meant to be used for any control loop that controls a DC value
 typedef struct GeneralPID
 {
   // setpoint for control routine
@@ -11,30 +12,117 @@ typedef struct GeneralPID
   float KP;
   float KI;
   float KD;
-  // Gains definition
+  // PID gains converted according to tustin discretization method
   float K1;
   float K2;
   float K3;
-  float K1_anti_windup;
-  float K2_anti_windup;
-  float K3_anti_windup;
-  // variables used to store past erros and outputs
-  float error_n1;
-  float error_n2;
-  float output_n1;
-  float output_n2;
-  // max output values
+  // anti-windup PD gains converted according to tustin discretization method
+  float K1_AW;
+  float K2_AW;
+  // define max and min output values
   float max_output;
   float min_output;
+  // variables used to store past erros and past outputs
+  float last_error;
+  float second_last_error;
+  float last_output;
+  float second_last_output;
   // pointers to functions
   int (*setSetpoint)(struct GeneralPID *self, float setpoint);
-  int (*setBoundaries)(struct GeneralPID *self, float max, float min);
-  float (*execute)(struct GeneralPID *self, float meas_variable);
+  float (*getSetpoint)(struct GeneralPID *self);
+  int (*setSamplingTime)(struct GeneralPID *self, float sampling_time);
+  float (*getSamplingTime)(struct GeneralPID *self);
+  int (*setGains)(struct GeneralPID *self, float kp, float ki, float kd);
+  void (*getGains)(struct GeneralPID *self, float *kp, float *ki, float *kd);
+  int (*setMatlabGains)(struct GeneralPID *self, float K1, float K2, float K3, float K1AW, float K2AW);
+  void (*getMatlabGains)(struct GeneralPID *self, float *K1, float *K2, float *K3, float *K1AW, float *K2AW);
+  int (*setLimits)(struct GeneralPID *self, float max, float min);
+  void (*getLimits)(struct GeneralPID *self, float *max, float *min);
+  float (*execute)(struct GeneralPID *self, float measurement);
 } GeneralPID;
 
-void GeneralPID_init(GeneralPID *self, float sampling_time, float kp, float ki, float kd);
-void GeneralPID_setGains(GeneralPID *self, float kp, float ki, float kd);
+/// @brief initialize the PID struct
+/// @param self pointer to the instance of the struct
+/// @param sampling_time sampling time (in seconds | ts = 1/f)
+/// @param kp proportional gain
+/// @param ki integral gain
+/// @param kd derivative gain
+/// @return 0 for success and 1 for errors
+int GeneralPID_init(GeneralPID *self, float sampling_time, float kp, float ki, float kd);
+
+/// @brief set the setpoint of the control loop
+/// @param self pointer to the instance of the struct
+/// @param setpoint numerical value of the setpoint
+/// @return 0 for success and 1 for errors
 int GeneralPID_setSetpoint(GeneralPID *self, float setpoint);
+
+/// @brief get setpoint value
+/// @param self pointer to the instance of the struct
+/// @return setpoint
+float GeneralPID_getSetpoint(GeneralPID *self);
+
+/// @brief set sampling time on the fly, this function also call GeneralPID_setGains
+/// @param self pointer to the instance of the struct
+/// @param sampling_time
+/// @return 0 for success and 1 for errors
+int GeneralPID_setSamplingTime(GeneralPID *self, float sampling_time);
+
+/// @brief get sampling time
+/// @param self pointer to the instance of the struct
+/// @return sampling time
+float GeneralPID_getSamplingTime(GeneralPID *self);
+
+/// @brief set the PID gains, converting them according to Tustin Discretization Method
+/// @param self pointer to the instance of the struct
+/// @param kp proportional gain
+/// @param ki integral gain
+/// @param kd derivative gain
+/// @return 0 for success and 1 for errors
+int GeneralPID_setGains(GeneralPID *self, float kp, float ki, float kd);
+
+/// @brief get PID gains through pointers
+/// @param self pointer to the instance of the struct
+/// @param kp a pointer to variable that will receive proportional gain
+/// @param ki a pointer to variable that will receive integral gain
+/// @param kd a pointer to variable that will receive derivative gain
+void GeneralPID_getGains(GeneralPID *self, float &kp, float &ki, float &kd);
+
+/// @brief set the gains according to matlab function c2d(model_tf, ts, 'tustin'), see examples for further explanation
+/// @param self pointer to the instance of the struct
+/// @param K1 value that multiplies z^2 -> K1 * z^2
+/// @param K2 value that multiplies z^1 -> K2 * z^1
+/// @param K3 value that multiplies z^0 -> K3 * z^0
+/// @param K1AW see anti-windup implementation for further explanation
+/// @param K2AW see anti-windup implementation for further explanation
+/// @return 0 for success and 1 for errors
+int GeneralPID_setMatlabGains(GeneralPID *self, float K1, float K2, float K3, float K1AW, float K2AW);
+
+/// @brief get PID gains through pointers
+/// @param self pointer to the instance of the struct
+/// @param K1 a pointer to variable that will receive K1 gain
+/// @param K2 a pointer to variable that will receive K2 gain
+/// @param K3 a pointer to variable that will receive K3 gain
+/// @param K1AW a pointer to variable that will receive K1AW gain
+/// @param K2AW a pointer to variable that will receive K2AW gain
+void GeneralPID_getMatlabGains(GeneralPID *self, float &K1, float &K2, float &K3, float &K1AW, float &K2AW);
+
+/// @brief set the maximum and minimum output of the control loop
+/// @param self pointer to the instance of the struct
+/// @param max maximum output value of GeneralPID_execute function
+/// @param min minimum output value of GeneralPID_execute function
+/// @return 0 for success and 1 for errors
+int GeneralPID_setLimits(GeneralPID *self, float max, float min);
+
+/// @brief get output limits
+/// @param self pointer to the instance of the struct
+/// @param max a pointer to variable that will receive the maximum output
+/// @param min a pointer to variable that will receive the minimum output
+void GeneralPID_getLimits(GeneralPID *self, float &max, float &min);
+
+/// @brief execute PID control loop
+/// @param self pointer to the instance of the struct
+/// @param measurement
+/// @return control loop action
 float GeneralPID_execute(GeneralPID *self, float measurement);
 
-#endif // fecha a diretiva de inclusão condicional
+#endif
