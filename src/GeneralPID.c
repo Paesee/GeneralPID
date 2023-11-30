e0a2500d3eca7d7f39b75e301286353ae81d7c78
Serramasther Projetos <projetos1@serramasther.local>
2023-11-30 17:03:06 -0300

    aaaa

--- src/GeneralPID.c
+++ src/GeneralPID.c
@@ -2,37 +2,37 @@
 
 int GeneralPID_init(GeneralPID *self, float sampling_time, float kp, float ki, float kd)
 {
-#include <stdlib.h> // For memory allocation functions
+  self->setpoint = 0;
+  self->sampling_time = sampling_time;
+  GeneralPID_setGains(self, kp, ki, kd);
+  self->max_output = 0;
+  self->min_output = 0;
+  self->last_error = 0;
+  self->second_last_error = 0;
+  self->last_output = 0;
+  self->second_last_output = 0;
+  self->setSetpoint = GeneralPID_setSetpoint;
+  self->getSetpoint = GeneralPID_getSetpoint;
+  self->setSamplingTime = GeneralPID_setSamplingTime;
+  self->getSamplingTime = GeneralPID_getSamplingTime;
+  self->setGains = GeneralPID_setGains;
+  self->getGains = GeneralPID_getGains;
+  self->setMatlabGains = GeneralPID_setMatlabGains;
+  self->getMatlabGains = GeneralPID_getMatlabGains;
+  self->setLimits = GeneralPID_setLimits;
+  self->getLimits = GeneralPID_getLimits;
+  self->execute = GeneralPID_execute;
+}
 
-  int GeneralPID_init(GeneralPID * self, float sampling_time, float kp, float ki, float kd)
-  {
-    if (self == 0)
-      return 1;
+int GeneralPID_setSetpoint(GeneralPID *self, float setpoint){
+  self->setpoint = setpoint;
+  return 0;
+}
 
-    // Initialize struct members with provided values
-    self->setpoint = 0.0;
-    self->sampling_time = sampling_time;
-    //GeneralPID_getGains;
-    self->max_output = 0.0;
-    self->min_output = 0.0;
-    self->last_error = 0.0;
-    self->second_last_error = 0.0;
-    self->last_output = 0.0;
-    self->second_last_output = 0.0;
+float GeneralPID_getSetpoint(GeneralPID *self){
+  return self->setpoint;
+}
 
-    // Set function pointers to zero
-    self->setSetpoint = 0;
-    self->getSetpoint = 0;
-    self->setSamplingTime = 0;
-    self->getSamplingTime = 0;
-    self->setGains = 0;
-    self->getGains = 0;
-    self->setMatlabGains = 0;
-    self->getMatlabGains = 0;
-    self->setLimits = 0;
-    self->getLimits = 0;
-    self->execute = 0;
-
-    return 0; // Success
-  }
+int GeneralPID_setSamplingTime(GeneralPID *self, float sampling_time){
+  
 }
